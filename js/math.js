753ebb9b0f01346c0feac949413941ae4559f7f7
agdavid <antonio.g.david@gmail.com>
2016-09-22 10:08:56 -0400

    add JS to solve using switch function

--- js/math.js
+++ js/math.js
@@ -1,5 +1,6 @@
 function doMath() {
   $('#solve').on('click', function() {
+    
     var num1 = parseInt($('#num1').val());
     var num2 = parseInt($('#num2').val());
     var operator = $('#operator').val();
@@ -12,14 +13,31 @@ function validate(num1, num2, operator) {
   if (operator === '+' || operator === '-' || operator === '*' || operator === '/') {
     if (isNaN(num1) || isNaN(num2)) {
       console.log('Invalid numbers! Must be real numbers.');
-      $('#result').append('Invalid numbers! Must be real numbers.');
+      $('#result').html('Invalid numbers! Must be real numbers.');
     } else {
-      // to be continued //
+      solve(num1, num2, operator);
     }    
   } else {
     console.log('Invalid operator! Must be +, -, *, or \/.');
-    $('#result').append('Invalid operator! Must be +, -, *, or \/.');
+    $('#result').html('Invalid operator! Must be +, -, *, or \/.');
   }   
+}
+
+function solve(num1, num2, operator) {
+  switch(operator) {
+    case '+':
+      $('#result').html((num1 + num2).toString());
+      break;
+    case '-':
+      $('#result').html((num1 - num2).toString());
+      break;
+    case '*':
+      $('#result').html((num1 * num2).toString());
+      break;
+    case '/':
+      $('#result').html((num1 / num2).toString());
+      break;
+  }
 }
 
 $(document).ready(function() {
