1f711a98d05aae94aca2b723b6c288cd93402bf5
agdavid <antonio.g.david@gmail.com>
2016-09-22 09:53:07 -0400

    add JS to validate operator

--- js/math.js
+++ js/math.js
@@ -1,13 +1,22 @@
 function doMath() {
+  $('#solve').on('click', function() {
+    var num1 = parseInt($('#num1').val());
+    var num2 = parseInt($('#num2').val());
+    var operator = $('#operator').val();
 
-  var num1 = $('#num1').val().parseInt();
-  var num2 = $('#num2').val().parseInt();
-  var operator = ('#operator').val();
+    validate(num1, num2, operator);
+  })
+}
 
-  result(num1, num2, operator);
-
+function validate(num1, num2, operator) {
+  if (operator === '+' || operator === '-' || operator === '*' || operator === '/') {
+      
+  } else {
+    console.log('Invalid operator! Must be +, -, *, or \/.')
+    $('#result').append('Invalid operator! Must be +, -, *, or \/.')
+  }   
 }
 
 $(document).ready(function() {
-
+  doMath();
 });
