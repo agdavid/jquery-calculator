123473fb8a4851b7639880e5293eb6e695aed15e
agdavid <antonio.g.david@gmail.com>
2016-09-22 09:40:23 -0400

    define doMath to set variables and pass to another function

--- js/math.js
+++ js/math.js
@@ -1,6 +1,12 @@
-var num1 = $('#num1').val();
-var num2 = $('#num2').val();
-var operator = ('#operator').val();
+function doMath() {
+
+  var num1 = $('#num1').val().parseInt();
+  var num2 = $('#num2').val().parseInt();
+  var operator = ('#operator').val();
+
+  result(num1, num2, operator);
+
+}
 
 $(document).ready(function() {
 
